88f0d71e55d5aaf86df6de637cf9cb2733c87c5f
ChrisPeng <pengchi1244@gmail.com>
2019-08-09 17:57:24 +0800

    Add firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,18 @@
 const Maze = require('amazejs');
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
+const firebaseConfig = {
+  apiKey: "AIzaSyAxGh07V0t-aTi5hvcYKEWOm6Ny-YLIKfg",
+  authDomain: "in-the-maze.firebaseapp.com",
+  databaseURL: "https://in-the-maze.firebaseio.com",
+  projectId: "in-the-maze",
+  storageBucket: "",
+  messagingSenderId: "492068311936",
+  appId: "1:492068311936:web:168d5bb5ff43f728"
+};
+firebase.initializeApp(firebaseConfig);
+auth();
 
 const size = 5;
 const canvas = document.getElementById('maze');
@@ -14,4 +28,15 @@ for (let row = 0; row < maze.height; row++) {
       pen.fillRect(col *  size, row * size, size, size);
     }
   }
+}
+
+function auth() {
+  firebase.auth().signInAnonymously().catch(console.error);
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(user);
+    } else {
+      console.log('user signed out');
+    }
+  });
 }
